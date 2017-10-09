0673297bdece2255d431bff260f49c2a1af1c3bd
Jack Scott <123.jack.scott@gmail.com>
2017-10-09 21:48:33 +1300

    Add project files.

---
+++ ttocskcajBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ttocskcajBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
