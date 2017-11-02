a603109b84e5852442a8ccd426bf98729e37df9e
Jack Scott <123.jack.scott@gmail.com>
2017-11-03 11:41:56 +1300

    Started adding some documentation

--- ttocskcajBot/Action.cs
+++ ttocskcajBot/Action.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ttocskcajBot.Entities.Things;
 
 namespace ttocskcajBot
 {
     internal class Action
     {
+        /// <summary>
+        /// A dictionary of properties that get changed on the Thing as a result to this action happening.
+        /// </summary>
         public Dictionary<string, object> Result { get; set; }
+
+        /// <summary>
+        /// The message to display to the user when this action happens.
+        /// </summary>
         public string Message { get; set; }
+
+        /// <summary>
+        /// Whether or not this action needs a Thing equipped to complete.
+        /// </summary>
         public bool NeedsEquippedThing { get; set; }
+
+        /// <summary>
+        /// Whether or not the Thing needs to be discovered before this action can happen.
+        /// </summary>
         public bool NeedsDiscovering { get; set; }
+
+        /// <summary>
+        /// A List of new Things that this action provides/creates when it happens. Dropped on the ground.
+        /// </summary>
+        public List<Thing> ProvidesThings { get; set; }
+
+        /// <summary>
+        /// Whether or not the Thing is destroyed when this action happens.
+        /// </summary>
+        public bool DestroysThing { get; set; }
     }
 }
--- ttocskcajBot/Entities/Things/Thing.cs
+++ ttocskcajBot/Entities/Things/Thing.cs
@@ -8,32 +8,94 @@ namespace ttocskcajBot.Entities.Things
 {
     internal class Thing : IEntity, ICloneable
     {
+        /// <summary>
+        /// The unique ID.
+        /// </summary>
         public string ID { get; set; }
+
+        /// <summary>
+        /// The Name
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// The description
+        /// </summary>
         public string Description { get; set; }
-        public string Category { get; set; }
+
+        /// <summary>
+        /// Whether or not the Thing can be taken by a player
+        /// </summary>
         public bool CanTake { get; set; }
+
+        /// <summary>
+        /// A message to display to the player when the Thing is equipped.
+        /// </summary>
         public string EquippedMessage { get; set; }
+
+        /// <summary>
+        /// Whether or not the Thing has been discovered.
+        /// </summary>
         public bool Discovered { get; set; }
+
+        /// <summary>
+        /// The amount of light the Thing produces.
+        /// </summary>
         public double LightLevel { get; set; }
+
+        /// <summary>
+        /// The state of the Thing. E.G. is it on or off?
+        /// </summary>
         public int State { get; set; }
+
+        /// <summary>
+        /// Alternative terms that the user can find this Thing with.
+        /// </summary>
         public string[] AlternativeTerms { get; set; }
+
+        /// <summary>
+        /// Actions that this thing is capable of.
+        /// </summary>
         public string[] ActionsAvailable { get; set; }
+
+        /// <summary>
+        /// Whether or not this Thing has limited uses.
+        /// </summary>
         public bool LimitedUses { get; set; }
+
+        /// <summary>
+        /// How many uses this Thing has remaining.
+        /// </summary>
         public int Uses { get; set; }
+
+        /// <summary>
+        /// The Actions that can be performed on this Thing.
+        /// </summary>
         public Dictionary<string, Action> ActionsReceivable { get; set; }
 
+        /// <summary>
+        /// Allows setting/getting of properties by their name using reflection.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to set/get</param>
+        /// <returns>The value of the specified property.</returns>
         public object this[string propertyName]
         {
             get => GetType().GetProperty(propertyName)?.GetValue(this, null);
             set => GetType().GetProperty(propertyName)?.SetValue(this, value, null);
         }
 
+        /// <summary>
+        /// Whether or not this Thing is currently a light source. I.E. Is it on, and does it provide light?
+        /// </summary>
+        /// <returns>True if the Thing is producing light.</returns>
         internal bool IsLightSource()
         {
             return State > 0 && LightLevel > 0;
         }
 
+        /// <summary>
+        /// A constructor that initializes some properties to default values.
+        /// </summary>
         public Thing()
         {
             // Initialize some default property values.
@@ -45,14 +107,22 @@ namespace ttocskcajBot.Entities.Things
 
         }
 
-
-
+        /// <summary>
+        /// Checks if this Thing matches a name/id provided or the provided name matches an alternativeTerm.
+        /// </summary>
+        /// <param name="entityName">The name to check.</param>
+        /// <returns>True if the Thing matches that name.</returns>
         public bool MatchesName(string entityName)
         {
             // Check if this things ID or AlternativeTerms matches entityName
             return ID.Equals(entityName) || AlternativeTerms.Contains(entityName);
         }
 
+        /// <summary>
+        /// Executes the provided action on this Thing.
+        /// </summary>
+        /// <param name="action">The Action to perform.</param>
+        /// <returns>A response to show the user.</returns>
         public string ExecuteAction(Action action)
         {
             // Change each property listed in the action results. Uses reflection and is probably slow :(
@@ -76,6 +146,11 @@ namespace ttocskcajBot.Entities.Things
             return response;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Creates a memberwise clone of this Thing.
+        /// </summary>
+        /// <returns>A new Thing object that is a clone of this one.</returns>
         public object Clone()
         {
             return MemberwiseClone();
